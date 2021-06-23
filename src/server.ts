dc3be509c2260ade7618a7daba7cfa07210530d9
leanfj <leanfj@hotmail.com>
2021-06-22 23:48:55 -0300

    criando banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
+import "reflect-metadata"
 import express, { response } from "express"
+import "./database"
 
 const app = express()
-
-app.get("/", (request, response) => response.send("Ola Leandro"))
-
-app.post("/rotapost", (request, response) => response.send("Rota post"))
 
 app.listen(3000, () => console.log(`Server running on http://localhost:3000`))
