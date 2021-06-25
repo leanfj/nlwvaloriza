1fd789d3442f6687d0991b822008c3420bcaaae6
Leandro Ferreira <leanfj@hotmail.com>
2021-06-24 23:28:49 -0300

    Tratamento de exceções

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
-import express, { response } from "express"
+import express, { NextFunction, Response, Request } from "express"
+import "express-async-errors"
 
 import { router } from "./routes"
 
@@ -9,6 +10,19 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
-const PORT = 3500
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
 
+  return response.status(500).json({
+    error: "Error",
+    message: "Internal Server Error"
+  })
+})
+
+
+const PORT = 3500
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
